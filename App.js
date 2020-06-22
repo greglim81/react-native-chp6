68aa616a252730b7d005b4da7b4bfdf594d0e4aa
Greg Lim <limjunqu@gmail.com>
2020-06-22 09:58:51 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Container, Header, Content, Body, Title } from 'native-base';
-import ProductList from './ProductList';
-import MyCard from './MyCard';
+import { Container } from 'native-base';
+import MyForm from './MyForm';
 
 export default function App() {
-
-  const isValid = true;
-
   return (
     <Container>
-      <MyCard>
-        This is a long sentence, and I want to insert content into the 
-        Card component from the outside.
-      </MyCard>
+      <MyForm />      
     </Container>
   );
 }
